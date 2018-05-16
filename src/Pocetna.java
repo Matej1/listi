a10a430b24a136435aa01df8e6a6914b22485e5e
Matej1 <matej.dislijeski94@gmail.com>
2018-05-16 20:57:51 +0200

    Prv commit

---
+++ src/Pocetna.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+public class Pocetna {
+
+	public static void main(String[] args) {
+		Vector<Integer> listaNaBroevi;
+		listaNaBroevi = new Vector<>();
+		listaNaBroevi.add(4);
+		listaNaBroevi.add(12);
+		listaNaBroevi.add(43);
+		listaNaBroevi.add(22);
+		listaNaBroevi.remove(0); 
+		List<Integer> pomaliOd50 = listaNaBroevi.stream()
+				.filter(x -> x<50)
+				.map(x -> x*15)
+				.collect(Collectors.toList());
+		System.out.println(pomaliOd50);
+	}
+	
+}
